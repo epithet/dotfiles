aaa9bc6d8d2f7f455506306e0f54b5a2daaeb47f
Sébastien Hoffmann <sog@i.net>
2023-03-22 22:18:30 +0100

    add nix user environment declaration

---
+++ .nix-defexpr/default.nix
@@ -0,0 +1,34 @@
+with import <nixpkgs> {}; [
+
+  alacritty # sakura wezterm foot
+  tab-rs zellij
+  starship
+  exa lsd bat ripgrep ripgrep-all fd
+  procs htop lsof
+  progress
+  tldr
+  jq
+  mdcat
+  binutils
+  bubblewrap
+
+  firefox surf # w3m lynx elinks browsh (jumanji?)
+  zathura
+  # mutt aerc
+  vlc
+
+  gnome.simple-scan
+
+  neovim
+  helix rust-analyzer lldb clang-tools rnix-lsp
+  gitui
+  git-ftp
+  meld
+
+  zettlr
+
+  restic
+  pass pass-git-helper
+  gnupg
+
+]
