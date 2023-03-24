f75dcc406e88110589c4137c039d15ef930c701c
Sébastien Hoffmann <sog@i.net>
2023-03-24 20:46:57 +0100

    add xfce4-panel.xml.tmpl

---
+++ .setup.sh
@@ -0,0 +1,4 @@
+# for files that conflate settings and volatile user data,
+# only a template is under version control
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml.tmpl \
+   ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
