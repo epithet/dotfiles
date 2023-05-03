1342a2573dee18ba6250a4dd0d523c722f4c1bf5
Sébastien Hoffmann <sog@i.net>
2023-05-03 15:42:05 +0200

    nvim: add treesitter playground

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -419,6 +419,32 @@ require('packer').startup(function(use)
             }
         end
     }
+    use {
+        "nvim-treesitter/playground",
+        cmd = {"TSPlaygroundToggle"},
+        config = function()
+            require "nvim-treesitter.configs".setup({
+                playground = {
+                    enable = true,
+                    disable = {},
+                    updatetime = 25, -- Debounced time for focussing in the playground from source
+                    persist_queries = false, -- Whether the query persists across vim sessions
+                    keybindings = {
+                        toggle_query_editor = 'o',
+                        toggle_hl_groups = 'i',
+                        toggle_injected_languages = 't',
+                        toggle_anonymous_nodes = 'a',
+                        toggle_language_display = 'I',
+                        focus_language = 'f',
+                        unfocus_language = 'F',
+                        update = 'R',
+                        goto_node = '<cr>',
+                        show_help = '?',
+                    },
+                }
+            })
+        end,
+    }
     -- }}} tree-sittter
 
     -- {{{ LSP
